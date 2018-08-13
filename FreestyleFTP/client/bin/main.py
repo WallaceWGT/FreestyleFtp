1d2f4ac557dae228c8b512247db0daf240dec293
WallaceWGT <1241499917@qq.com>
2018-08-13 10:14:33 +0800

    first commit

---
+++ FreestyleFTP/client/bin/main.py
@@ -0,0 +1,10 @@
+"--wallace--"
+# 客户器端程序入口
+
+import os
+import sys
+
+BASE_DIR = os.path.dirname(os.path.dirname((__file__)))
+
+sys.path.append(BASE_DIR)
+
---
+++ FreestyleFTP/sever/bin/main.py
@@ -0,0 +1,14 @@
+"--wallace--"
+
+# 服务器端程序入口
+
+import os
+import sys
+
+# BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+
+# sys.path.append(BASE_DIR)
+
+
+import sev
+
